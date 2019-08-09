c91485d84020e826504288a6748571f3891381d7
Mukesh <tsmukesh99@gmail.com>
2019-08-09 18:26:52 +0530

    2 templates done

--- starhub-report-automate/src/app/app.module.ts
+++ starhub-report-automate/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MnFullpageModule } from "ngx-fullpage";
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { AgendaComponent } from './agenda/agenda.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    AgendaComponent
   ],
   imports: [
     BrowserModule,
